de7ccf0aa5527ac273eb55a68a296c7dc00b74f1
moisesmcardona <moises@moises-studios.com>
2018-06-09 11:03:56 -0400

    Code refactoring for the SendMentionEmail.py script

--- SendMentionEmail.py
+++ SendMentionEmail.py
@@ -15,16 +15,83 @@ MySQLDB2 = 'drupal_database'
 MySQLUsername = 'username'
 MySQLPass = 'password'
 
+def insertMentionInMySQL(post_author, post_link, mention_string):
+    conn = MySQLdb.connect(host=MySQLHost, db=MySQLDB, user=MySQLUsername, passwd=MySQLPass)
+    query = "INSERT INTO mentions (author, link, username, time) VALUES (%s, %s, %s, %s)"
+    cursor = conn.cursor()
+    cursor.execute(query, (post_author, post_link, mention_string, time.strftime("%Y/%m/%d %H:%M:%S")))
+    conn.commit()
+    conn.close()
+    print(mention_string)
+
+def getSteemPlaceUserFromMentionString(mention_string):
+    conn = MySQLdb.connect(host=MySQLHost, db=MySQLDB, user=MySQLUsername, passwd=MySQLPass)
+    query = "SELECT DISTINCT drupalkey, notifyenabled FROM settings WHERE username = '%s'" % (mention_string)
+    print("query set")
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchone()
+
+def getSteemPlaceUserEmailAndLanguage(key_to_use):
+    conn = MySQLdb.connect(host=MySQLHost, db=MySQLDB2, user=MySQLUsername, passwd=MySQLPass)
+    query = "SELECT DISTINCT mail, language FROM users WHERE uid = %d" % (key_to_use)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchone()
+
+def getPostOrComment(parent_author):
+    if parent_author == '':
+        postOrComment = "post"
+    else:
+        postOrComment = "comentario"
+    return postOrComment
+
+def sendMail(record2, mention_string, post_author, post_link):
+    mailtouse = record2[0]
+    languagetosend = record2[1]
+    print("Email found")
+    print("begin email")
+    msg = MIMEMultipart()
+    # Send email
+    msg['From'] = 'noreply@steem.place'
+    msg['To'] = mailtouse
+    full_link = "https://steemit.com/tag/@" + post_author + "/" + post_link
+    print(languagetosend)
+    if "es" in languagetosend:
+        print("sending email in spanish")
+        postOrComment = getPostOrComment(parent_author)
+        messagetitle = "@" + mention_string + ", has sido mencionado en un " + postOrComment + " de @" + post_author
+        messagetosend = "Hola, <a href=https://steemit.com/@" + mention_string + ">@" + mention_string + "</a><br><br>Has sido mencionado en el siguiente " + postOrComment + " de <a href=https://steemit.com/@" + post_author + ">@" + post_author + "</a>:<br><br><a href=" + full_link + ">" + full_link + "</a><br><br>Atentamente,<br>Steem.Place"
+    else:
+        print("sending email in english")
+        postOrComment = getPostOrComment(parent_author)
+        messagetitle = "@" + mention_string + ", you have been mentioned in a " + postOrComment + " by @" + post_author
+        messagetosend = "Hi, <a href=https://steemit.com/@" + mention_string + ">@" + mention_string + "</a><br><br>You have been mentioned in the following " + postOrComment + " by <a href=https://steemit.com/@" + post_author + ">@" + post_author + "</a>:<br><br><a href=" + full_link + ">" + full_link + "</a><br><br>Sincerely,<br>Steem.Place"
+    msg['Subject'] = messagetitle
+    message = messagetosend
+    msg.attach(MIMEText(message, 'html'))
+    mailserver = smtplib.SMTP('smtp-relay.gmail.com', 587)
+    # identify ourselves to smtp gmail client
+    mailserver.ehlo()
+    # secure our email with tls encryption
+    mailserver.starttls()
+    # re-identify ourselves as an encrypted connection
+    mailserver.ehlo()
+    # mailserver.login('me@gmail.com', 'mypassword')
+    mailserver.sendmail('noreply@steem.place', mailtouse, msg.as_string())
+    mailserver.quit()
+    print("Email sent")
+
 while True:
     try:
         for op in chain.stream(filter_by=["comment"]):
             parent_author = op["parent_author"]
-            postauthor = op["author"]
-            postlink = op["permlink"]
+            post_author = op["author"]
+            post_link = op["permlink"]
             wordarray = op["body"].split(' ')
             mentionlist = []
             for word in wordarray:
-                mentionstring = ""
+                mention_string = ""
                 getstring = False
                 for i in range(0, len(word)):
                     letters = set("<>|/\!#$%^&*()=+,'?")
@@ -33,92 +100,32 @@ while True:
                     if "\n" in word[i]:
                         break
                     if getstring == True:
-                        mentionstring = mentionstring + word[i]
+                        mention_string = mention_string + word[i]
                     if "@" in word[i]:
                         getstring = True
-                if mentionstring != "":
-                    if not "@" in mentionstring:
-                        mentionstring = mentionstring.lower()
-                        if mentionstring.endswith('.'):
-                            mentionstring = mentionstring[:-1]
-                        if not mentionstring in mentionlist:
-                            conn = MySQLdb.connect(host=MySQLHost, db=MySQLDB, user=MySQLUsername, passwd=MySQLPass)
-                            query = "INSERT INTO mentions (author, link, username, time) VALUES (%s, %s, %s, %s)"
-                            cursor = conn.cursor()
-                            cursor.execute(query,(postauthor, postlink, mentionstring, time.strftime("%Y/%m/%d %H:%M:%S")))
-                            conn.commit()
-                            conn.close()
-                            print(mentionstring)
+                if mention_string != "":
+                    if not "@" in mention_string:
+                        mention_string = mention_string.lower()
+                        if mention_string.endswith('.'):
+                            mention_string = mention_string[:-1]
+                        if not mention_string in mentionlist:
+                            insertMentionInMySQL(post_author, post_link, mention_string)
                             try:
-                                conn = MySQLdb.connect(host=MySQLHost, db=MySQLDB, user=MySQLUsername, passwd=MySQLPass)
-                                query = "SELECT DISTINCT drupalkey, notifyenabled FROM settings WHERE username = '%s'" % (mentionstring)
-                                print("query set")
-                                cursor = conn.cursor()
-                                cursor.execute(query)
-                                record1 = cursor.fetchone()
-                                keytouse = 0
-                                enabled = 0
+                                record1 = getSteemPlaceUserFromMentionString(mention_string)
                                 found = False
                                 print("query executed. Getting Record")
                                 if record1 is not None:
                                     found = True
                                     print("checking")
-                                    keytouse = record1[0]
+                                    key_to_use = record1[0]
                                     enabled = record1[1]
-                                if found == True:
+                                if found:
                                     print("user found 1")
                                     if enabled == 1:
                                         print("email enabled")
-                                        conn = MySQLdb.connect(host=MySQLHost, db=MySQLDB2, user=MySQLUsername,passwd=MySQLPass)
-                                        query = "SELECT DISTINCT mail, language FROM users WHERE uid = %d" % (keytouse)
-                                        cursor = conn.cursor()
-                                        cursor.execute(query)
-                                        record2 = cursor.fetchone()
-                                        mailtouse = ""
-                                        languagetosend = ""
+                                        record2 = getSteemPlaceUserEmailAndLanguage(key_to_use)
                                         if record2 is not None:
-                                            mailtouse = record2[0]
-                                            languagetosend = record2[1]
-                                            print("Email found")
-                                            print("begin email")
-                                            msg = MIMEMultipart()
-                                            # Send email
-                                            msg['From'] = 'noreply@steem.place'
-                                            msg['To'] = mailtouse
-                                            messagetosend = ""
-                                            messagetitle = ""
-                                            fulllink = "https://steemit.com/tag/@" + postauthor + "/" + postlink
-                                            print(languagetosend)
-                                            if "es" in languagetosend:
-                                                print("sending email in spanish")
-                                                if parent_author == '':
-                                                    postOrComment = "post"
-                                                else:
-                                                    postOrComment = "comentario"
-                                                messagetitle = "@" + mentionstring + ", has sido mencionado en un " + postOrComment + " de @" + postauthor
-                                                messagetosend = "Hola, <a href=https://steemit.com/@" + mentionstring + ">@" + mentionstring + "</a><br><br>Has sido mencionado en el siguiente " + postOrComment + " de <a href=https://steemit.com/@" + postauthor + ">@" + postauthor + "</a>:<br><br><a href=" + fulllink + ">" + fulllink + "</a><br><br>Atentamente,<br>Steem.Place"
-                                            else:
-                                                print("sending email in english")
-                                                if parent_author == '':
-                                                    postOrComment = "post"
-                                                else:
-                                                    postOrComment = "comment"
-                                                messagetitle = "@" + mentionstring + ", you have been mentioned in a " + postOrComment + " by @" + postauthor
-                                                messagetosend = "Hi, <a href=https://steemit.com/@" + mentionstring + ">@" + mentionstring + "</a><br><br>You have been mentioned in the following " + postOrComment + " by <a href=https://steemit.com/@" + postauthor + ">@" + postauthor + "</a>:<br><br><a href=" + fulllink + ">" + fulllink + "</a><br><br>Sincerelly,<br>Steem.Place"
-                                            msg['Subject'] = messagetitle
-                                            message = messagetosend
-                                            msg.attach(MIMEText(message, 'html'))
-                                            mailserver = smtplib.SMTP('smtp-relay.gmail.com', 587)
-                                            # identify ourselves to smtp gmail client
-                                            mailserver.ehlo()
-                                            # secure our email with tls encryption
-                                            mailserver.starttls()
-                                            # re-identify ourselves as an encrypted connection
-                                            mailserver.ehlo()
-                                            # mailserver.login('me@gmail.com', 'mypassword')
-                                            mailserver.sendmail('noreply@steem.place', mailtouse, msg.as_string())
-                                            mailserver.quit()
-                                            print("Email sent")
+                                            sendMail(record2, mention_string, post_author, post_link)
                                         else:
                                             print("Email address not found")
                                     else:
@@ -127,7 +134,7 @@ while True:
                                     print("Record not found")
                             except Exception as inst:
                                 print(inst)
-                        mentionlist.append(mentionstring)
+                        mentionlist.append(mention_string)
             mentionlist.clear()
     except Exception as inst:
         print(inst)
